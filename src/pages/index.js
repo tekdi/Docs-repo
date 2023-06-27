af5c22702fea249f1e09639e9f75dd0df889fe6c
prasad <prasad.harke@tekditechnologies.com>
2023-06-27 20:38:53 +0530

    changes done

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,10 +16,9 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            //className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+             </Link>
         </div>
       </div>
     </header>
